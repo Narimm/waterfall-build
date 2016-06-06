ba6537314c920e695662345b7261cd37770c370f
Techcable <Techcable@techcable.net>
2016-06-06 13:36:10 -0600

    Don't send KICK packets while in HANDSHAKE state

--- proxy/src/main/java/net/md_5/bungee/connection/InitialHandler.java
+++ proxy/src/main/java/net/md_5/bungee/connection/InitialHandler.java
@@ -565,7 +565,7 @@ public class InitialHandler extends PacketHandler implements PendingConnection
     @Override
     public void disconnect(final BaseComponent... reason)
     {
-        if ( thisState != State.STATUS && thisState != State.PING )
+        if ( thisState != State.STATUS && thisState != State.PING && thisState != State.HANDSHAKE ) // Waterfall: Don't kick during handshake
         {
             ch.delayedClose( new Kick( ComponentSerializer.toString( reason ) ) );
         } else
