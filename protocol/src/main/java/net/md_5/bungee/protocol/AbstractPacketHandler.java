1e6fcb1787d778bef1f15008b157b90168fb1c9f
Aaron Hill <aa1ronham@gmail.com>
2016-09-15 22:38:37 +0200

    Fix potion race condition on Forge 1.8.9

--- protocol/src/main/java/net/md_5/bungee/protocol/AbstractPacketHandler.java
+++ protocol/src/main/java/net/md_5/bungee/protocol/AbstractPacketHandler.java
@@ -7,6 +7,8 @@ import net.md_5.bungee.protocol.packet.ClientStatus;
 import net.md_5.bungee.protocol.packet.Commands;
 import net.md_5.bungee.protocol.packet.EncryptionRequest;
 import net.md_5.bungee.protocol.packet.EncryptionResponse;
+import net.md_5.bungee.protocol.packet.EntityEffect; // Waterfall
+import net.md_5.bungee.protocol.packet.EntityRemoveEffect; // Waterfall
 import net.md_5.bungee.protocol.packet.EntityStatus;
 import net.md_5.bungee.protocol.packet.Handshake;
 import net.md_5.bungee.protocol.packet.KeepAlive;
@@ -168,4 +170,14 @@ public abstract class AbstractPacketHandler
     public void handle(Commands commands) throws Exception
     {
     }
+
+    // Waterfall start
+    public void handle(EntityEffect entityEffect) throws Exception
+    {
+    }
+
+    public void handle(EntityRemoveEffect removeEffect) throws Exception
+    {
+    }
+    // Waterfall end
 }
