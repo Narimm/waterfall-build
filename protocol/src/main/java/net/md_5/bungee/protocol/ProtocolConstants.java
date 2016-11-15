2f2e304ee44890946cef72c37fca2cf5f0750942
Troy Frew <fuzzy_bot@arenaga.me>
2016-11-15 09:07:51 -0500

    Fixup ProtocolConstants

--- protocol/src/main/java/net/md_5/bungee/protocol/ProtocolConstants.java
+++ protocol/src/main/java/net/md_5/bungee/protocol/ProtocolConstants.java
@@ -61,6 +61,16 @@ public class ProtocolConstants
             ProtocolConstants.MINECRAFT_1_15_1
     );
 
+    public static final boolean isBeforeOrEq(int before, int other)
+    {
+            return before <= other;
+    }
+
+    public static final boolean isAfterOrEq(int after, int other)
+    {
+            return after >= other;
+    }
+
     public enum Direction
     {
 
