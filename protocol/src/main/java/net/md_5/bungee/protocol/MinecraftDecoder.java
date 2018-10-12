024a9bbf355de8679e241042f3b5d7f6ab9b000a
Shane Freeder <theboyetronic@gmail.com>
2018-10-12 14:28:52 +0100

    Ignore empty packets
    This patch puts the proxy more inline with the client in that empty
    packets will be ignored. While empty packets are a sign of bad plugins,
    they are effectivly harmless vs the cost of the exception in general

--- protocol/src/main/java/net/md_5/bungee/protocol/MinecraftDecoder.java
+++ protocol/src/main/java/net/md_5/bungee/protocol/MinecraftDecoder.java
@@ -36,6 +36,12 @@ public class MinecraftDecoder extends MessageToMessageDecoder<ByteBuf>
         Object packetTypeInfo = null;
         try
         {
+            // Waterfall start
+            if (in.readableBytes() == 0) {
+                return;
+            }
+            // Waterfall end
+
             int packetId = DefinedPacket.readVarInt( in );
             packetTypeInfo = packetId;
 
