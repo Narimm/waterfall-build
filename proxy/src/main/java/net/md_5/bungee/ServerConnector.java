WaterfallMC/Waterfall-Old

Optional server list ping logging.
Add IPs to the log where user names are shown.

This avoids spamming the logs with connection notices.
Server list pings are only logged if the log_server_list_pings config.yml option is true, defaults to false

proxy/src/main/java/net/md_5/bungee/ServerConnector.java
@@ -348,6 +348,6 @@ public class ServerConnector extends PacketHandler
     @Override
     public String toString()
     {
-        return "[" + user.getName() + "] <-> ServerConnector [" + target.getName() + "]";
+        return "[" + user.getName() + "|" + user.getName() + "] <-> ServerConnector [" + target.getName() + "]";
     }
 }

Narimm/waterfall-build

proxy/src/main/java/net/md_5/bungee/ServerConnector.java
@@ -405,6 +405,6 @@ public class ServerConnector extends PacketHandler
     @Override
     public String toString()
     {
-        return "[" + user.getName() + "] <-> ServerConnector [" + target.getName() + "]";
+        return "[" + user.getName() + "|" + user.getAddress() + "] <-> ServerConnector [" + target.getName() + "]";
     }
 }