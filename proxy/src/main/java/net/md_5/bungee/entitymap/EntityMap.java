9e0c7a1b107bab19a5ed80ddc7df5a68fbde4410
Shane Freeder <theboyetronic@gmail.com>
2019-01-14 03:35:21 +0000

    Provide an option to disable entity metadata rewriting
    The work here is derived from the research and guidance of
    various members of the minecraft community
    
    This patch provides the ability to disable entity remapping,
    which creates various incompatability issues with mods, however,
    may also create various issues with mods which do not support this,
    hence why the configuration option is provided

--- proxy/src/main/java/net/md_5/bungee/entitymap/EntityMap.java
+++ proxy/src/main/java/net/md_5/bungee/entitymap/EntityMap.java
@@ -6,6 +6,7 @@ import io.netty.buffer.ByteBufInputStream;
 import java.io.DataInputStream;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+
 import net.md_5.bungee.protocol.DefinedPacket;
 import net.md_5.bungee.protocol.ProtocolConstants;
 import se.llbit.nbt.NamedTag;
@@ -27,6 +28,11 @@ public abstract class EntityMap
     // Returns the correct entity map for the protocol version
     public static EntityMap getEntityMap(int version)
     {
+        // Waterfall start
+        if (net.md_5.bungee.api.ProxyServer.getInstance().getConfig().isDisableEntityMetadataRewrite()) {
+            return EntityMap_Dummy.INSTANCE;
+        }
+        // Waterfall end
         switch ( version )
         {
             case ProtocolConstants.MINECRAFT_1_8:
@@ -280,7 +286,13 @@ public abstract class EntityMap
                     DefinedPacket.readVarInt( packet );
                     break;
                 default:
-                    throw new IllegalArgumentException( "Unknown meta type " + type );
+                    // Waterfall start - Don't lie
+                    if ( protocolVersion >= ProtocolConstants.MINECRAFT_1_13 )
+                    {
+                        type++;
+                    }
+                    throw new IllegalArgumentException( "Unknown meta type " + type  + ": Using mods? refer to disable_entity_metadata_rewrite in waterfall.yml" );
+                    // Waterfall end
             }
         }
 
